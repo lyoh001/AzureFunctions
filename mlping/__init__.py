1d0b47db9734369f2204d415067fb004a64cd8ba
lyoh001@hotmail.com <lyoh001@hotmail.com>
2022-11-06 11:03:42 +1100

    Commit

---
+++ mlping/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+import azure.functions as func
+import requests
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    logging.info("*******Starting main function*******")
+    logging.info(
+        requests.post(
+            "https://white-water-02d382d10.2.azurestaticapps.net/api/mlsupplychain",
+            json={
+                "customer_id": ["789203"],
+                "days_for_delivery": ["1"],
+                "order_date": ["2022-01-01"],
+                "order_qty": ["10"],
+                "product_name": ["AM Ghee 250"],
+            },
+        )
+    )
