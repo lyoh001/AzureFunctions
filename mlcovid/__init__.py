2d61c95d44f06ede28516a110106e652284d163f
John Yohan Lee <45280343+lyoh001@users.noreply.github.com>
2022-12-30 11:00:50 +1100

    Commit

---
+++ mlcovid/__init__.py
@@ -0,0 +1,48 @@
+import logging
+import os
+import warnings
+
+import azure.functions as func
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from tensorflow import keras
+
+warnings.filterwarnings("ignore")
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+model = keras.models.load_model("mlcovid/model")
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("*******Starting main function*******")
+    logging.info(f"Request query: {req.get_json()}")
+    payload = {
+        k: [np.nan] if next(iter(v)) == "" else v for k, v in req.get_json().items()
+    }
+    try:
+        image_id = next(iter(payload["image_id"]))
+        if not 1 <= int(image_id) <= 30:
+            return func.HttpResponse(
+            status_code=200,
+            body="Invalid input, please use numbers between 1 and 30.",
+        )
+    except ValueError:
+        return func.HttpResponse(
+            status_code=200,
+            body="Invalid input, please use numbers between 1 and 30.",
+        )
+
+    # normal = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28]
+    # virus = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29]
+    # covid = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+    logging.info(np.array(Image.open(os.path.join("mlcovid/images", f"{image_id}.jpg"))))
+
+    # prediction = model.predict(preprocessor.transform(X))[0][0]
+    # logging.info(X)
+    # logging.info(f"prediction: {prediction}")
+
+    return func.HttpResponse(
+        status_code=200,
+        body="test"
+        # body=f"{prediction}"
+    )
