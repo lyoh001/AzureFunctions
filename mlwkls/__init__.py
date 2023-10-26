1523a9ae7c376b6659ad67370d4fa4d4e2822ee7
John Yohan Lee <45280343+lyoh001@users.noreply.github.com>
2023-10-27 07:41:45 +1100

    Commit

--- mlwkls/__init__.py
+++ mlwkls/__init__.py
@@ -1,39 +1,289 @@
 import logging
 import os
+import shutil
+from io import BytesIO
 
 import azure.functions as func
-import openai
+from PyPDF2 import PdfReader, PdfWriter
+from reportlab.pdfgen import canvas
+
+max_width = 480
+input_pdf_path = "input_pdf.pdf"
+output_pdf_path = "output_pdf_with_text.pdf"
+
+
+def load_template(id: str) -> str:
+    src_path = "./mlwkls/template"
+    dst_path = os.path.join("/tmp", id)
+    if not os.path.exists(dst_path):
+        os.mkdir(dst_path)
+    for item in os.listdir(src_path):
+        item_path = os.path.join(src_path, item)
+        if os.path.isdir(item_path):
+            shutil.copytree(item_path, os.path.join(dst_path, item))
+        else:
+            shutil.copy2(item_path, dst_path)
+    return dst_path
+
+
+def get_rating(rating):
+    return {
+        "0": "Needs Attention",
+        "1": "Satisfactory",
+        "2": "Very Good",
+        "3": "Excellent",
+    }.get(rating, "Unknown")
+
+
+def add_text_to_pdf(input_path, output_path, text_content, x, y, max_width, font_size):
+    pdf_reader = PdfReader(input_path)
+    pdf_writer = PdfWriter()
+
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        packet = BytesIO()
+        c = canvas.Canvas(packet)
+        c.setFont("Helvetica", font_size)
+        lines = []
+        current_line = ""
+        text_lines = text_content.split("\n")
+        for line in text_lines:
+            words = line.split()
+            for word in words:
+                test_line = current_line + word + " "
+                width = c.stringWidth(test_line, "Helvetica", font_size)
+                if width <= max_width:
+                    current_line = test_line
+                else:
+                    lines.append(current_line.strip())
+                    current_line = word + " "
+            lines.append(current_line.strip())
+            current_line = ""
+
+        text_x = x
+        text_y = y
+
+        for line in lines:
+            c.drawString(text_x, text_y, line)
+            text_y -= font_size * 1.2
+        c.save()
+        packet.seek(0)
+        modified_page = PdfReader(packet).pages[0]
+        page.merge_page(modified_page)
+        pdf_writer.add_page(page)
+
+    with open(output_path, "wb") as output_file:
+        pdf_writer.write(output_file)
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("*******Starting brush function*******")
 
     try:
-        text = req.get_json()
-        context = f"name: {text['name']}\ngrade: {text['grade']}\nAttendance: {text['studentAttendance']}\nskills: {' '. join(text['skills'])}"
-        report = openai.ChatCompletion.create(
-            engine=os.environ["OPENAI_DEPLOYMENT_NAME"],
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {
-                    "role": "user",
-                    "content": "Can you write a concise student annual report?",
+        user_input = req.get_json()
+        student_name = user_input["studentName"]
+        grade = user_input["grade"]
+        attendance = user_input["attendance"]
+        effort = user_input["effort"]
+        behavior = user_input["behavior"]
+        listening_previous_semester = user_input["listeningPreviousSemester"]
+        listening_current_semester = user_input["listeningCurrentSemester"]
+        listening_skills = user_input["listeningSkills"]
+        reading_previous_semester = user_input["readingPreviousSemester"]
+        reading_current_semester = user_input["readingCurrentSemester"]
+        reading_skills = user_input["readingSkills"]
+        speaking_previous_semester = user_input["speakingPreviousSemester"]
+        speaking_current_semester = user_input["speakingCurrentSemester"]
+        speaking_skills = user_input["speakingSkills"]
+        writing_previous_semester = user_input["writingPreviousSemester"]
+        writing_current_semester = user_input["writingCurrentSemester"]
+        writing_skills = user_input["writingSkills"]
+        overall_comment = user_input["overallComment"]
+
+        path = load_template(student_name)
+
+        text = f"{student_name} / Year {grade}"
+        text_x = 160
+        text_y = 702
+        add_text_to_pdf(
+            os.path.join(path, input_pdf_path),
+            os.path.join(path, output_pdf_path),
+            text,
+            text_x,
+            text_y,
+            max_width,
+            11,
+        )
+
+        text = get_rating(attendance)
+        text_x = 160
+        text_y = 678
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            text,
+            text_x,
+            text_y,
+            max_width,
+            11,
+        )
+
+        text = get_rating(effort)
+        text_x = 420
+        text_y = 702
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            text,
+            text_x,
+            text_y,
+            max_width,
+            11,
+        )
+
+        text = get_rating(behavior)
+        text_x = 420
+        text_y = 678
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            text,
+            text_x,
+            text_y,
+            max_width,
+            11,
+        )
+
+        text = "X---------------------O"
+        text_x = 335
+        text_y = 183
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            text,
+            text_x,
+            text_y,
+            max_width,
+            11,
+        )
+
+        text = "X---------------------O"
+        text_x = 335
+        text_y = 161
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            text,
+            text_x,
+            text_y,
+            max_width,
+            11,
+        )
+
+        text = "X---------------------O"
+        text_x = 335
+        text_y = 141
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            text,
+            text_x,
+            text_y,
+            max_width,
+            11,
+        )
+
+        text = "X---------------------O"
+        text_x = 335
+        text_y = 120
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            text,
+            text_x,
+            text_y,
+            max_width,
+            11,
+        )
+
+        text_x = 60
+        text_y = 625
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            "\n".join(listening_skills),
+            text_x,
+            text_y,
+            max_width,
+            9,
+        )
+
+        text_x = 60
+        text_y = 538
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            "\n".join(reading_skills),
+            text_x,
+            text_y,
+            max_width,
+            9,
+        )
+
+        text_x = 60
+        text_y = 450
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            "\n".join(speaking_skills),
+            text_x,
+            text_y,
+            max_width,
+            9,
+        )
+
+        text_x = 60
+        text_y = 360
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            "\n".join(writing_skills),
+            text_x,
+            text_y,
+            max_width,
+            9,
+        )
+
+        text_x = 60
+        text_y = 276
+        add_text_to_pdf(
+            os.path.join(path, output_pdf_path),
+            os.path.join(path, output_pdf_path),
+            overall_comment,
+            text_x,
+            text_y,
+            max_width,
+            9,
+        )
+
+        if os.path.exists(os.path.join(path, output_pdf_path)):
+            with open(os.path.join(path, output_pdf_path), "rb") as file:
+                file_content = file.read()
+            return func.HttpResponse(
+                body=file_content,
+                headers={
+                    "Content-Disposition": "attachment; filename=report.pdf",
+                    "Content-Type": "application/pdf",
                 },
-                {
-                    "role": "assistant",
-                    "content": "Sure, please provide me with the information you would like to include in the student annual report.",
-                },
-                {
-                    "role": "user",
-                    "content": f"Sure, here are the student details: {context}\nThe report should only include 2~3 short comments.",
-                },
-            ],
-        )["choices"][0]["message"]["content"]
-        logging.info(f"context:\n{context}")
-        logging.info(f"report:\n{report}")
+                status_code=200,
+            )
+        else:
+            return func.HttpResponse("Error generating the report", status_code=500)
 
     except ValueError:
         return func.HttpResponse(
-            status_code=400, body="Invalid input, please provide a valid text."
+            "Invalid input, please provide a valid user_input.", status_code=400
         )
-    return func.HttpResponse(report, status_code=200)
+
+    except Exception as e:
+        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
